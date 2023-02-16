3edd67329837f95bc24d6853df546cdd3e7668b4
sbr-vincent <vincent.g.sbr@gmail.com>
2023-02-16 15:04:50 -0800

    CSS Changes

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
-            <h1>Firt Post</h1>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
             <h2><Link href="/" >Back to home.</Link></h2>
-        </>
+        </Layout>
     )
 }
